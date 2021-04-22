536fcd9025cdec270854fabdb7949da280694cd4
罗鑫 <270639090@qq.com>
2021-04-22 19:15:48 +0800

    11

--- nodePractice/01.node/03_module.js
+++ nodePractice/01.node/03_module.js
@@ -1,7 +1,8 @@
 // 引入模块
 
-// let md = require("./02_module.js")
-let md1 = require("../work/add.js")
-console.log(md1.addFun(1,3));
-console.log(md1.mulFun(1,3));
-
+let md = require("./02_module.js")
+// let md1 = require("../work/add.js")
+// console.log(md1.addFun(1,3));
+// console.log(md1.mulFun(1,3));
+// console.log(global)
+console.log(arguments.callee + "");
