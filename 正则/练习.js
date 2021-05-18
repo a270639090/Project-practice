87a118c32aa29ec2445398d5e223d256f2b793ef
luo xin@2042 <270639090@qq.com>
2021-05-18 10:36:27 +0800

    新增正则

---
+++ 正则/练习.js
@@ -0,0 +1,9 @@
+// 匹配16进制颜色
+// let regex = /#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})/g
+// let str = '#aacc #aa48c4 #aa44c5 #abbec4 #a84'
+// console.log(str.match(regex));
+
+// 匹配时间
+// let regex = /^(([01][1-9]|[2][0-3]):[0-5][0-9])|([2][4]:[0]+)$/g
+// let str = '19:03 30:15 12:00'
+// console.log(regex.test('12:00'));
